68bf4c819aae70eb284707a920729a4313fbf3b9
James McCarthy <james.mccarthy@uwaterloo.ca>
2015-03-11 10:18:50 -0400

    Lots of routing and security fixes

--- js/goose.js
+++ js/goose.js
@@ -1,14 +1,12 @@
 //Globals
 var map;
-var gooseNestsSubmitURL = "http://services2.arcgis.com/rEyyACsbHLGwNRQS/arcgis/rest/services/gw_submissions/FeatureServer/0?token=_tRUxNA5B9tklre8Hqwf3bHYERJoIshc6albG6Gx2LJLTusEPAN6J7T2dheUKvJWj065AXobNRpKPg1_Jq4R3Ej-3nt_M-qiueZNBJ1e7nFqJ-qoFnHv6OTnuv8vwAGrpmZ3F7R1MfvPhOIMBJKhGg..";
-var gooseNestsViewURL = "http://services2.arcgis.com/rEyyACsbHLGwNRQS/arcgis/rest/services/GooseWatch_Public/FeatureServer/0";
+var gooseNestsURL = "http://services2.arcgis.com/rEyyACsbHLGwNRQS/arcgis/rest/services/gw_submissions/FeatureServer/0?token=";
 var geometryServiceURL = "http://env-gisdev1.uwaterloo.ca:6080/arcgis/rest/services/Utilities/Geometry/GeometryServer";
-var routeTaskURL = "http://env-gisdev1.uwaterloo.ca:6080/arcgis/rest/services/Campus/uw_route/NAServer/Route"; //TODO: Token, publish somewhere permanent
+var routeTaskURL = "http://env-gisdev.uwaterloo.ca/arcgis_public/rest/services/goosewatch15/gw15_route/NAServer/Route";
 var extentLayerURL = "https://services1.arcgis.com/DwLTn0u9VBSZvUPe/arcgis/rest/services/UW_Buildings/FeatureServer/0";
 var uwBldgsURL = "https://api.uwaterloo.ca/v2/buildings/list.json?key=cb63602dd1fd2a14332405f8613b68ed&output=json&callback=populateBuildings&jsonp=?";
-var submittedPicsURL = "http://services2.arcgis.com/rEyyACsbHLGwNRQS/arcgis/rest/services/GooseWatch_Picture_Submissions/FeatureServer/0?token=_tRUxNA5B9tklre8Hqwf3bHYERJoIshc6albG6Gx2LJLTusEPAN6J7T2dheUKvJWj065AXobNRpKPg1_Jq4R3Ej-3nt_M-qiueZNBJ1e7nFqJ-qoFnHv6OTnuv8vwAGrpmZ3F7R1MfvPhOIMBJKhGg..";
+var submittedPicsURL = "http://services2.arcgis.com/rEyyACsbHLGwNRQS/arcgis/rest/services/GooseWatch_Picture_Submissions/FeatureServer/0?token=";
 var gooseFL;
-var gooseEditFL;
 var x, y;
 var offCampusBuildings = ["AAC","AAR","PHR","ARC","GA","HSC","WSS","180King","RAC","RA2"];
 var buildings;
@@ -29,212 +27,221 @@ var routeAttributionText = "Esri Canada, MappedIn, ";
 var routeAttribution = "";
 var submittedPicsFL;
 
-require(["esri/map", "esri/arcgis/utils","esri/layers/FeatureLayer","esri/tasks/FeatureSet","esri/layers/GraphicsLayer","esri/symbols/SimpleMarkerSymbol","esri/symbols/SimpleLineSymbol","esri/tasks/RouteTask","esri/tasks/RouteParameters","esri/tasks/GeometryService","esri/tasks/query","esri/geometry/webMercatorUtils","esri/dijit/PopupTemplate","esri/dijit/PopupMobile","esri/renderers/SimpleRenderer","esri/renderers/ScaleDependentRenderer","esri/symbols/PictureMarkerSymbol","dojo/dom-construct","dojo/domReady!"], function(Map,arcgisUtils,FeatureLayer,FeatureSet,GraphicsLayer,SimpleMarkerSymbol,SimpleLineSymbol,RouteTask,RouteParameters,GeometryService,Query,webMercatorUtils,PopupTemplate,PopupMobile,SimpleRenderer,ScaleDependentRenderer,PictureMarkerSymbol,domConstruct){
+require(["esri/map", "esri/config", "esri/arcgis/utils","esri/layers/FeatureLayer","esri/tasks/FeatureSet","esri/layers/GraphicsLayer","esri/symbols/SimpleMarkerSymbol","esri/symbols/SimpleLineSymbol","esri/tasks/RouteTask","esri/tasks/RouteParameters","esri/tasks/GeometryService","esri/tasks/query","esri/geometry/webMercatorUtils","esri/dijit/PopupTemplate","esri/dijit/PopupMobile","esri/renderers/SimpleRenderer","esri/renderers/ScaleDependentRenderer","esri/symbols/PictureMarkerSymbol","dojo/dom-construct","dojo/domReady!"], function(Map,esriConfig,arcgisUtils,FeatureLayer,FeatureSet,GraphicsLayer,SimpleMarkerSymbol,SimpleLineSymbol,RouteTask,RouteParameters,GeometryService,Query,webMercatorUtils,PopupTemplate,PopupMobile,SimpleRenderer,ScaleDependentRenderer,PictureMarkerSymbol,domConstruct){
 
-    //Handle tokens
-    var routeToken;
-    var gwToken;
-    $.get('tokens.txt',function(data){
-        var the_url = document.URL;
-        var lines = data.split("\r\n");
-        $.each(lines,function(i,v){
-            var tokens = v.split(",");
-            if (document.URL.indexOf(tokens[0]) > -1){
-                routeToken = tokens[1];
-                gwToken = tokens[2];
-            }
-        });
-    });
+  // Proxy stuff
+  // esriConfig.defaults.io.proxyUrl = "http://env-gis-srv1.uwaterloo.ca/proxy/proxy.ashx";
+  //esriConfig.defaults.io.proxyUrl = "http://env-gisdev1.uwaterloo.ca/proxy.ashx";
+  //esriConfig.defaults.io.proxyUrl = "/proxy.ashx";
 
+  esriConfig.defaults.io.corsEnabledServers.push("env-gisdev.uwaterloo.ca");
 
+  $('#carousel').carousel();
 
-    $('#carousel').carousel();
+  var $modals = $('.modal')
+  $modals.on('hidden.bs.modal',function(e){
+    unPauseEtiquetteCarousel();
+  });
+  $modals.on('shown.bs.modal',function(e){
+    pauseEtiquetteCarousel();
+  });
 
-    var $modals = $('.modal')
-    $modals.on('hidden.bs.modal',function(e){
-        unPauseEtiquetteCarousel();
-    });
-    $modals.on('shown.bs.modal',function(e){
-        pauseEtiquetteCarousel();
-    });
+  $('#sharePanel').hide()
 
-    $('#sharePanel').hide()
+  gsvc = new GeometryService(geometryServiceURL);
 
-	gsvc = new GeometryService(geometryServiceURL);
+  //Get token from AGOL
+  var agol_token;
+  $.ajax({
+    type: "POST",
+    url: "https://www.arcgis.com/sharing/rest/oauth2/token/",
+    data: {
+      client_id: 'rttBmoUarS4C5dM4',
+      client_secret: '1fb4f0a97ca249208066f6c8b2a4405f',
+      grant_type: 'client_credentials'
+    },
+    success: function(data){
+      agol_token = JSON.parse(data)['access_token'];
 
-	//Create nested feature set to hold nest buffers at three levels
-	buildings = new FeatureSet();
-	nestBuffers.push(new esri.tasks.FeatureSet());
-	nestBuffers.push(new esri.tasks.FeatureSet());
-	nestBuffers.push(new esri.tasks.FeatureSet());
+      //console.log(agol_token);
+      gooseNestsURL += agol_token;
+      submittedPicsURL += agol_token;
 
-	//Create the map
-	map = new Map("mapDiv",{
-		basemap: "topo",
-		center: [-80.542, 43.471],
-		zoom: 15,
+      //Create nested feature set to hold nest buffers at three levels
+      buildings = new FeatureSet();
+      nestBuffers.push(new esri.tasks.FeatureSet());
+      nestBuffers.push(new esri.tasks.FeatureSet());
+      nestBuffers.push(new esri.tasks.FeatureSet());
+
+      //Create the map
+      map = new Map("mapDiv",{
+        basemap: "topo",
+        center: [-80.542, 43.471],
+        zoom: 15,
         minZoom: 15,
         maxZoom: 19
-	});
+      });
 
-	//Create a new lat/long point when the user clicks on the map (if in add point mode)
-	map.on("click",function(ev){
-		if(addPointMode){
-			var webmercPt = ev.mapPoint;
-			var llPt = webMercatorUtils.webMercatorToGeographic(webmercPt);
-			populateLocationFromClick(llPt);
-		}
-	});
+      //Create a new lat/long point when the user clicks on the map (if in add point mode)
+      map.on("click",function(ev){
+        if(addPointMode){
+          var webmercPt = ev.mapPoint;
+          var llPt = webMercatorUtils.webMercatorToGeographic(webmercPt);
+          populateLocationFromClick(llPt);
+        }
+      });
 
-    //Modify the data attribution text when the map changes
-    map.on("extent-change",function(ev){
+      //Modify the data attribution text when the map changes
+      map.on("extent-change",function(ev){
         $(".esriAttributionLastItem").text(routeAttribution +  $(".esriAttributionLastItem").text());
-    });
+      });
 
 
-	//Initialize map contents once map has loaded
-	map.on("load", initLayers);
-
-
-
-	/**
-		initLayers()
-
-		Initializes all the layers after the map has loaded
-	*/
-	function initLayers(){
-		//get reference to submitted pictures
-        submittedPicsFL = new FeatureLayer(submittedPicsURL);
-
-        //Goose nest symbol
-        var smallNestSymbol = new PictureMarkerSymbol("img/NestLocationsGoose.svg",30,30);
-        var largeNestSymbol = new PictureMarkerSymbol("img/NestLocationsGoose.svg",50,50);
-        var smallNestRenderer = new SimpleRenderer(smallNestSymbol);
-        var largeNestRenderer = new SimpleRenderer(largeNestSymbol);
-
-        var rendererInfos = [
-            {
-                "renderer": smallNestRenderer,
-                "minZoom": 15,
-                "maxZoom":16
-            },
-            {
-                "renderer": largeNestRenderer,
-                "minZoom":17,
-                "maxZoom":19
-            }
-        ];
-
-        var scaleDependentRenderer = new ScaleDependentRenderer();
-        scaleDependentRenderer.setRendererInfos(rendererInfos);
-		//Create the goose nest feature layer
+      //Initialize map contents once map has loaded
+      map.on("load", initLayers);
+    }
+  })
 
 
 
-		//Using MODE_SELECTION because it's the only way I can find to make sure the nests are loaded before making buffers
-		gooseFL = new FeatureLayer(gooseNestsViewURL,{
-			mode: FeatureLayer.MODE_SELECTION,
-			outFields: ["*"]
-		});
-        gooseFL.setRenderer(scaleDependentRenderer);
 
-		//When the features are selected create the nest buffers
-		gooseFL.on("selection-complete",makeNestBuffers);
 
-    gooseEditFL = new FeatureLayer(gooseNestsSubmitURL,{
+
+
+
+  /**
+  initLayers()
+
+  Initializes all the layers after the map has loaded
+  */
+  function initLayers(){
+    //get reference to submitted pictures
+    submittedPicsFL = new FeatureLayer(submittedPicsURL);
+
+    //Goose nest symbol
+    var smallNestSymbol = new PictureMarkerSymbol("img/NestLocationsGoose.svg",30,30);
+    var largeNestSymbol = new PictureMarkerSymbol("img/NestLocationsGoose.svg",50,50);
+    var smallNestRenderer = new SimpleRenderer(smallNestSymbol);
+    var largeNestRenderer = new SimpleRenderer(largeNestSymbol);
+
+    var rendererInfos = [
+    {
+      "renderer": smallNestRenderer,
+      "minZoom": 15,
+      "maxZoom":16
+    },
+    {
+      "renderer": largeNestRenderer,
+      "minZoom":17,
+      "maxZoom":19
+    }
+    ];
+
+    var scaleDependentRenderer = new ScaleDependentRenderer();
+    scaleDependentRenderer.setRendererInfos(rendererInfos);
+    //Create the goose nest feature layer
+
+
+
+    //Using MODE_SELECTION because it's the only way I can find to make sure the nests are loaded before making buffers
+    gooseFL = new FeatureLayer(gooseNestsURL,{
       mode: FeatureLayer.MODE_SELECTION,
       outFields: ["*"]
     });
+    gooseFL.setRenderer(scaleDependentRenderer);
 
-		//Add attachments if they exist once a new feature is created
-		gooseEditFL.on("edits-complete",attachPhoto);
-        //Add attachments when a new photo is submitted
-        submittedPicsFL.on("edits-complete",attachNewPhoto);
+    //When the features are selected create the nest buffers
+    gooseFL.on("selection-complete",makeNestBuffers);
 
-        //Create a new feature layer to hold an extent based on UW building footprints
-        extentLayer = new FeatureLayer(extentLayerURL);
+    //Add attachments if they exist once a new feature is created
+    gooseFL.on("edits-complete",attachPhoto);
+    //Add attachments when a new photo is submitted
+    submittedPicsFL.on("edits-complete",attachNewPhoto);
 
-		//This is based on the forum post here: http://forums.arcgis.com/threads/77989-Display-Image-Attachments-in-Popup
-		gooseFL.on("click",function(e){
-            pauseEtiquetteCarousel();
-			var objectId, el;
-			objectId = e.graphic.attributes[gooseFL.objectIdField];
-			gooseFL.queryAttachmentInfos(objectId, function (infos) {
+    //Create a new feature layer to hold an extent based on UW building footprints
+    extentLayer = new FeatureLayer(extentLayerURL);
 
-
-				var d = new Date(e.graphic.attributes.submitdate);
-
-				$("#nestDate")[0].innerHTML = d.toLocaleDateString();
-				$("#nestDescription")[0].innerHTML = e.graphic.attributes.description;
-				$("#nestSubmitter")[0].innerHTML = e.graphic.attributes.submitter;
-				$("#nestTwitter")[0].innerHTML = e.graphic.attributes.twitter;
-                $("#nestOID")[0].value = e.graphic.attributes.OBJECTID;
-                $("#nestX")[0].value = e.graphic.geometry.x;
-                $("#nestY")[0].value = e.graphic.geometry.y;
+    //This is based on the forum post here: http://forums.arcgis.com/threads/77989-Display-Image-Attachments-in-Popup
+    gooseFL.on("click",function(e){
+      pauseEtiquetteCarousel();
+      var objectId, el;
+      objectId = e.graphic.attributes[gooseFL.objectIdField];
+      gooseFL.queryAttachmentInfos(objectId, function (infos) {
 
 
-                $("#nestImagePlaceholder").hide();
+        var d = new Date(e.graphic.attributes.submitdate);
 
-				if (!!infos[0]) {
-                     if(!!$("#carouselSlides").children()){
-                        $("#carouselSlides").children().remove();
-                    }
-
-                        $("#nestImagePlaceholder").show();
-
-                        for (var i=0; i < infos.length; i++){
-                            var slide = document.createElement('div');
-                            var attvalue = i==0 ? "item active" : "item";
-                            slide.setAttribute('class',attvalue);
-                            var el = document.createElement('img');
-                            el.setAttribute('src', infos[i].url);
-                            //el.setAttribute('style','width:90%');
-                            slide.appendChild(el);
-                            $("#carouselSlides").append(slide);
-                        }
-                        if (infos.length == 1){
-                            $('.carousel-inner').each(function() {
-                                if ($(this).children('div').length === 1) $(this).siblings('.carousel-control, .carousel-indicators').hide();
-                            });
-                        }
-                        else{
-                             $('.carousel-inner').each(function() {
-                                if ($(this).children('div').length > 1) $(this).siblings('.carousel-control, .carousel-indicators').show();
-                            });
-                        }
-                    }
-					$("#nestModal").modal("show");
-
-			});
-		});
-
-		map.addLayer(gooseFL);
+        $("#nestDate")[0].innerHTML = d.toLocaleDateString();
+        $("#nestDescription")[0].innerHTML = e.graphic.attributes.description;
+        $("#nestSubmitter")[0].innerHTML = e.graphic.attributes.submitter;
+        $("#nestTwitter")[0].innerHTML = e.graphic.attributes.twitter;
+        $("#nestOID")[0].value = e.graphic.attributes.OBJECTID;
+        $("#nestX")[0].value = e.graphic.geometry.x;
+        $("#nestY")[0].value = e.graphic.geometry.y;
 
 
+        $("#nestImagePlaceholder").hide();
 
-		//Create graphics layers to hold buffer graphics and route results
-		results = new GraphicsLayer();
-		bufferGraphics = new GraphicsLayer();
+        if (!!infos[0]) {
+          if(!!$("#carouselSlides").children()){
+            $("#carouselSlides").children().remove();
+          }
 
-		//Set up routing parameters
-		routeTask = new RouteTask(routeTaskURL);
-		routeParams = new RouteParameters();
-		routeParams.stops = new FeatureSet();
-		routeParams.polygonBarriers = new FeatureSet();
-		routeParams.returnPolygonBarriers = true;
-		routeParams.outSpatialReference = {"wkid":102100};
-		routeParams.doNotLocateOnRestrictedElements = true;
+          $("#nestImagePlaceholder").show();
 
-		//Routing events
-		routeTask.on("solve-complete",showRoute);
-		routeTask.on("error",routeErrorHandler);
+          for (var i=0; i < infos.length; i++){
+            var slide = document.createElement('div');
+            var attvalue = i==0 ? "item active" : "item";
+            slide.setAttribute('class',attvalue);
+            var el = document.createElement('img');
+            el.setAttribute('src', infos[i].url);
+            //el.setAttribute('style','width:90%');
+            slide.appendChild(el);
+            $("#carouselSlides").append(slide);
+          }
+          if (infos.length == 1){
+            $('.carousel-inner').each(function() {
+              if ($(this).children('div').length === 1) $(this).siblings('.carousel-control, .carousel-indicators').hide();
+            });
+          }
+          else{
+            $('.carousel-inner').each(function() {
+              if ($(this).children('div').length > 1) $(this).siblings('.carousel-control, .carousel-indicators').show();
+            });
+          }
+        }
+        $("#nestModal").modal("show");
 
-		//Set up routing symbology
-        stopSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.STYLE_CIRCLE, 15, new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, new dojo.Color([0,0,0]), 1), new dojo.Color([255,0,0,0.25]));
-		routeSymbol = new SimpleLineSymbol().setColor(new dojo.Color([21,35,115,0.75])).setWidth(4);
+      });
+    });
+
+    map.addLayer(gooseFL);
 
 
-		// Get building data from uWaterloo API
+
+    //Create graphics layers to hold buffer graphics and route results
+    results = new GraphicsLayer();
+    bufferGraphics = new GraphicsLayer();
+
+    //Set up routing parameters
+    routeTask = new RouteTask(routeTaskURL);
+    routeParams = new RouteParameters();
+    routeParams.stops = new FeatureSet();
+    routeParams.polygonBarriers = new FeatureSet();
+    routeParams.returnPolygonBarriers = true;
+    routeParams.outSpatialReference = {"wkid":102100};
+    routeParams.doNotLocateOnRestrictedElements = true;
+
+    //Routing events
+    routeTask.on("solve-complete",showRoute);
+    routeTask.on("error",routeErrorHandler);
+
+    //Set up routing symbology
+    stopSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.STYLE_CIRCLE, 15, new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, new dojo.Color([0,0,0]), 1), new dojo.Color([255,0,0,0.25]));
+    routeSymbol = new SimpleLineSymbol().setColor(new dojo.Color([21,35,115,0.75])).setWidth(4);
+
+
+    // Get building data from uWaterloo API
 
     var uw_api_key = "cb63602dd1fd2a14332405f8613b68ed"; //TODO: Get this from text file
     var bldg_url = "http://api.uwaterloo.ca/v2/buildings/list.json?key=" + uw_api_key;
@@ -249,25 +256,25 @@ require(["esri/map", "esri/arcgis/utils","esri/layers/FeatureLayer","esri/tasks/
     });
 
 
-		//Add graphics layers to map
-		map.addLayer(results);
-		//map.addLayer(bufferGraphics,0);
+    //Add graphics layers to map
+    map.addLayer(results);
+    //map.addLayer(bufferGraphics,0);
 
-		//Select all nests to get them on the map
-		var selectAll = new Query;
-		// selectAll.where = '"status"=0';
-    selectAll.where = '1=1';
-		gooseFL.selectFeatures(selectAll,FeatureLayer.SELECTION_NEW);
+    //Select all nests to get them on the map
+    var selectAll = new Query;
+    selectAll.where = '"status"=1';
+    // selectAll.where = '1=1';
+    gooseFL.selectFeatures(selectAll,FeatureLayer.SELECTION_NEW);
 
-        centerMapURL();
+    centerMapURL();
 
-	}
+  }
 });
 
 function pauseEtiquetteCarousel(){
-    $("#carousel").carousel('pause');
+  $("#carousel").carousel('pause');
 }
 
 function unPauseEtiquetteCarousel(){
-    $("#carousel").carousel('cycle');
+  $("#carousel").carousel('cycle');
 }
--- js/util.js
+++ js/util.js
@@ -1,17 +1,17 @@
 /**
-	locateUser()
-	
-	Starts the HTML geolocation process and (provided it is supported) uses the returned value to populate the location
-	on the submission form.
+locateUser()
+
+Starts the HTML geolocation process and (provided it is supported) uses the returned value to populate the location
+on the submission form.
 */
 function locateUser(){
-	if(navigator.geolocation){  
+	if(navigator.geolocation){
 		navigator.geolocation.getCurrentPosition(populateLocationFromDevice,function(error){
-            alert("Unable to find your position:" + error.message);
-        },{enableHighAccuracy:true, timeout:30000});
+			alert("Unable to find your position:" + error.message);
+		},{enableHighAccuracy:true, timeout:30000});
 		watchId = navigator.geolocation.watchPosition(populateLocationFromDevice,function(error){
-            console.log("Unable to find your position:" + error.message);
-        },{enableHighAccuracy:true, timeout:30000});
+			console.log("Unable to find your position:" + error.message);
+		},{enableHighAccuracy:true, timeout:30000});
 	}
 	else{
 		$("#locationLink")[0].style.color = "red";
@@ -20,9 +20,9 @@ function locateUser(){
 }
 
 /**
-	sortSelect()
-	
-	Sorts the option elements in a select element based on their text content
+sortSelect()
+
+Sorts the option elements in a select element based on their text content
 */
 function sortSelect(selElem) {
 	var tmpAry = new Array();
@@ -43,57 +43,57 @@ function sortSelect(selElem) {
 }
 
 function centerMapURL(){
-require(["esri/geometry/Point"],function(Point){
-    if (location.search != ""){
-        //Get query portion of URL (if any)
-        var url_query = location.search.split("?")[1];
-        var x,y,useButtons,zoom;
-        var pt;
-        if (url_query !== ""){
-            $.each(url_query.split("&"),function(i,v){
-                key = v.split("=")[0];
-                value = v.split("=")[1];
-                if(key=="x"){
-                    x = value;
-                }
-                else if (key=="y"){
-                    y = value;
-                }
-                else if (key=="btns"){
-                    useButtons = value;
-                }
-                else if (key=="zoom"){
-                    zoom = value;
-                }
-            });
+	require(["esri/geometry/Point"],function(Point){
+		if (location.search != ""){
+			//Get query portion of URL (if any)
+			var url_query = location.search.split("?")[1];
+			var x,y,useButtons,zoom;
+			var pt;
+			if (url_query !== ""){
+				$.each(url_query.split("&"),function(i,v){
+					key = v.split("=")[0];
+					value = v.split("=")[1];
+					if(key=="x"){
+						x = value;
+					}
+					else if (key=="y"){
+						y = value;
+					}
+					else if (key=="btns"){
+						useButtons = value;
+					}
+					else if (key=="zoom"){
+						zoom = value;
+					}
+				});
 
 
-            if (useButtons == "false"){
-                $("#shareButton").hide();
-                $("#submitButton").hide();
-                $("#routeButton").hide();
-            }
+				if (useButtons == "false"){
+					$("#shareButton").hide();
+					$("#submitButton").hide();
+					$("#routeButton").hide();
+				}
 
-            //make sure both x and y have values
-            if (!!x && !!y){
-                pt = new Point([x,y]);
-                map.centerAndZoom(pt,zoom);
-            }
+				//make sure both x and y have values
+				if (!!x && !!y){
+					pt = new Point([x,y]);
+					map.centerAndZoom(pt,zoom);
+				}
 
-            
-        }
-    }
-});}
 
-function generateShareURL(){
-require(["esri/geometry/webMercatorUtils"],function(webMercatorUtils){
-    var mapcenter = map.extent.getCenter();
-    var geo_pt = webMercatorUtils.webMercatorToGeographic(mapcenter);
-    var x = geo_pt.x.toFixed(3);
-    var y = geo_pt.y.toFixed(3);
-    var zoom = map.getZoom();
-    var shareURL = document.URL + "?x=" + x + "&y=" + y + "&zoom=" + zoom;
-    $("#shareURL")[0].value = shareURL;
-    //alert(shareURL);
-    $("#sharePanel").show();
-});}
+			}
+		}
+	});}
+
+	function generateShareURL(){
+		require(["esri/geometry/webMercatorUtils"],function(webMercatorUtils){
+			var mapcenter = map.extent.getCenter();
+			var geo_pt = webMercatorUtils.webMercatorToGeographic(mapcenter);
+			var x = geo_pt.x.toFixed(3);
+			var y = geo_pt.y.toFixed(3);
+			var zoom = map.getZoom();
+			var shareURL = document.URL + "?x=" + x + "&y=" + y + "&zoom=" + zoom;
+			$("#shareURL")[0].value = shareURL;
+			//alert(shareURL);
+			$("#sharePanel").show();
+		});}
