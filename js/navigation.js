04c0fad0c8eff4232ae78f4972f040fcab728295
James McCarthy <james.mccarthy@uwaterloo.ca>
2015-03-11 10:18:50 -0400

    Lots of routing and security fixes

--- js/navigation.js
+++ js/navigation.js
@@ -1,11 +1,11 @@
 /**
-	populateBuildings()
-	
-	Goes through the JSON response of building points, builds the 2 pick lists,
-	and the featureset object to hold all buildings not in the offCampusBuildings list
+populateBuildings()
+
+Goes through the JSON response of building points, builds the 2 pick lists,
+and the featureset object to hold all buildings not in the offCampusBuildings list
 */
 function populateBuildings(data){
-	
+
 	//Loop through buildings returned from the API
 	dojo.forEach(data.data,function(value,index){
 		bldg = value.building_code;
@@ -14,7 +14,7 @@ function populateBuildings(data){
 			bldgDesc = value.building_code + " (" + value.building_name + ")";
 			dojo.byId("buildingSelectorA").options.add(new Option(bldgDesc,bldg));
 			dojo.byId("buildingSelectorB").options.add(new Option(bldgDesc,bldg));
-			
+
 			//Build a graphic feature for the building location
 			var pt = new esri.geometry.Point(value.longitude,value.latitude);
 			var sym = new esri.symbol.SimpleMarkerSymbol().setStyle(esri.symbol.SimpleMarkerSymbol.STYLE_SQUARE).setColor(new dojo.Color([255,0,0,0.5]));
@@ -23,216 +23,216 @@ function populateBuildings(data){
 			buildings.features.push(b);
 		}
 	});
-	
+
 	//Sort the two lists based on their text content
 	sortSelect(document.getElementById('buildingSelectorA'));
-    dojo.byId("buildingSelectorA").options.add(new Option("Use My Location","gps"),0);
-    $("#buildingSelectorA").val("gps")
+	dojo.byId("buildingSelectorA").options.add(new Option("Use My Location","gps"),0);
+	$("#buildingSelectorA").val("gps")
 	sortSelect(document.getElementById('buildingSelectorB'));
 }
 
 /**
-    makeNestBuffers()
-    
-    Uses a geometry service to Generate a set of buffer polygons at three distances (specified in the bufferParams.distances array).
-    Adds the buffer graphics corresponding to the current selection on the fear level drop-down as barriers on the route.
-    Adds graphics for all buffers to a nested list (nestBuffers) for quick retrieval.
+makeNestBuffers()
+
+Uses a geometry service to Generate a set of buffer polygons at three distances (specified in the bufferParams.distances array).
+Adds the buffer graphics corresponding to the current selection on the fear level drop-down as barriers on the route.
+Adds graphics for all buffers to a nested list (nestBuffers) for quick retrieval.
 */
 function makeNestBuffers() {
 	require(["esri/symbols/SimpleFillSymbol","esri/symbols/SimpleLineSymbol","esri/tasks/GeometryService","esri/SpatialReference","esri/tasks/BufferParameters","esri/graphic","esri/InfoTemplate"],function(SimpleFillSymbol,SimpleLineSymbol,GeometryService,SpatialReference,BufferParameters,Graphic,InfoTemplate){
-        
-        
-        //Build the list of descriptions for screen readers
-        //Using the approach documented here: http://webaim.org/techniques/css/invisiblecontent/
-        $("#nestDescriptions")[0].innerHTML = "There are reported goose nests at the following locations: <br/>";
-        $.each(gooseFL.graphics,function(i,v){
-            $("#nestDescriptions")[0].innerHTML += v.attributes.LocDescrip + ";" ;
-        });
-        
-        //Symbol for nests
-        var sfs = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, new dojo.Color([255, 255, 255]), 1),new dojo.Color([210, 105, 30, 0.9]));
-			
-        dojo.forEach(gooseFL.graphics, function(nest,index){
-            var pt = nest.geometry;
-            //Build buffers
-            //Set up buffer parameters
-            bufferParams = new BufferParameters();
-            bufferParams.distances = [ 12, 20, 30 ];
-            bufferParams.unit = GeometryService.UNIT_METER;
-            bufferParams.bufferSpatialReference = new SpatialReference({wkid: 102100});
-            bufferParams.outSpatialReference = map.spatialReference;			
-            bufferParams.geometries  = [ pt ];
-				
-            //create a function that returns a function that will call the geometry service on demand
-            var buildInfoTemplate = function(nestDesc){
-                return function(bufResults){
-                    var bufGraphic = new Graphic(bufResults[$("#fearSelector").val()],sfs);
-                    //routeParams.polygonBarriers.features.push(bufGraphic);	
-                    var bufInfoTemplate = new InfoTemplate();
-                    //nestBuffers.features.push(bufGraphic);
-                    bufferGraphics.add(bufGraphic);
-                    nestBuffers[0].features.push(new Graphic(bufResults[0],sfs));
-                    nestBuffers[1].features.push(new Graphic(bufResults[1],sfs));
-                    nestBuffers[2].features.push(new Graphic(bufResults[2],sfs));
-                };
-            };
-				
+
+
+		//Build the list of descriptions for screen readers
+		//Using the approach documented here: http://webaim.org/techniques/css/invisiblecontent/
+		$("#nestDescriptions")[0].innerHTML = "There are reported goose nests at the following locations: <br/>";
+		$.each(gooseFL.graphics,function(i,v){
+			$("#nestDescriptions")[0].innerHTML += v.attributes.LocDescrip + ";" ;
+		});
+
+		//Symbol for nests
+		var sfs = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, new dojo.Color([255, 255, 255]), 1),new dojo.Color([210, 105, 30, 0.9]));
+
+		dojo.forEach(gooseFL.graphics, function(nest,index){
+			var pt = nest.geometry;
+			//Build buffers
+			//Set up buffer parameters
+			bufferParams = new BufferParameters();
+			bufferParams.distances = [ 12, 20, 30 ];
+			bufferParams.unit = GeometryService.UNIT_METER;
+			bufferParams.bufferSpatialReference = new SpatialReference({wkid: 102100});
+			bufferParams.outSpatialReference = map.spatialReference;
+			bufferParams.geometries  = [ pt ];
+
+			//create a function that returns a function that will call the geometry service on demand
+			var buildInfoTemplate = function(nestDesc){
+				return function(bufResults){
+					var bufGraphic = new Graphic(bufResults[$("#fearSelector").val()],sfs);
+					//routeParams.polygonBarriers.features.push(bufGraphic);
+					var bufInfoTemplate = new InfoTemplate();
+					//nestBuffers.features.push(bufGraphic);
+					bufferGraphics.add(bufGraphic);
+					nestBuffers[0].features.push(new Graphic(bufResults[0],sfs));
+					nestBuffers[1].features.push(new Graphic(bufResults[1],sfs));
+					nestBuffers[2].features.push(new Graphic(bufResults[2],sfs));
+				};
+			};
+
 			//Buffer the features and add the results as barriers to the routing task
 			gsvc.buffer(bufferParams,buildInfoTemplate(""));
-        });
-    });
+		});
+	});
 }
 
 /**
-	validateAndAddStops()
-	
-	Validates building selections on routing modal and if they are valid adds them as stops to the network.
+validateAndAddStops()
+
+Validates building selections on routing modal and if they are valid adds them as stops to the network.
 */
 function validateAndAddStops(){
-require(["esri/tasks/FeatureSet"],function(FeatureSet){
-	//Clear old routes
-	results.clear();
-	
-	//Clear old stops
-	routeParams.stops = new FeatureSet();
-	
-	//What are the start and end buildings?
-	var buildingA = dojo.byId("buildingSelectorA").value;
-	var buildingB = dojo.byId("buildingSelectorB").value;
-	
-	//if this stays true, find a route
-	var findRoute = true;
-    
-    //if this becomes true use the device location as the start instead of a building
-    useDeviceLoc = false;
-	
-    //Handle the case where a user sets their start location to their current device location
-    if (buildingA == "gps"){
-        useDeviceLoc = true;
-        //locateUser();
-        if(navigator.geolocation){  
-            navigator.geolocation.getCurrentPosition(addStopFromDeviceLocation,function(error){
-                alert("Unable to find your position:" + error.message);
-                routeParams.stops = new FeatureSet();
-            },{enableHighAccuracy:true, timeout:30000});
-        }
-        else{
-            alert("Browser doesn't support Geolocation. Visit http://caniuse.com to discover browser support for the Geolocation API.");
-        }
-    }
-    
-	//User has not specified a building
-	if (buildingA == ""){
-		alert("Please select a valid start location");
-		findRoute = false;
-	}
-	if (buildingB == ""){
-		alert("Please select a valid end location");
-		findRoute = false;
-	}
-	
-	//Make sure buildings aren't the same
-	if (buildingA == buildingB && buildingA != ""){
-		alert("Your start and end location must be different");
-		findRoute = false;
-	}
-	
-	//All is OK above, find the route
-	if (findRoute){
-        dojo.forEach(buildings.features, function(value,index){
-            if (value.attributes.Acronym == buildingA || value.attributes.Acronym == buildingB){
-                //When a building is found add it as a stop
-                addStop(value);
-            }
-        });
-	}
-});
+	require(["esri/tasks/FeatureSet"],function(FeatureSet){
+		//Clear old routes
+		results.clear();
+
+		//Clear old stops
+		routeParams.stops = new FeatureSet();
+
+		//What are the start and end buildings?
+		var buildingA = dojo.byId("buildingSelectorA").value;
+		var buildingB = dojo.byId("buildingSelectorB").value;
+
+		//if this stays true, find a route
+		var findRoute = true;
+
+		//if this becomes true use the device location as the start instead of a building
+		useDeviceLoc = false;
+
+		//Handle the case where a user sets their start location to their current device location
+		if (buildingA == "gps"){
+			useDeviceLoc = true;
+			//locateUser();
+			if(navigator.geolocation){
+				navigator.geolocation.getCurrentPosition(addStopFromDeviceLocation,function(error){
+					alert("Unable to find your position:" + error.message);
+					routeParams.stops = new FeatureSet();
+				},{enableHighAccuracy:true, timeout:30000});
+			}
+			else{
+				alert("Browser doesn't support Geolocation. Visit http://caniuse.com to discover browser support for the Geolocation API.");
+			}
+		}
+
+		//User has not specified a building
+		if (buildingA == ""){
+			alert("Please select a valid start location");
+			findRoute = false;
+		}
+		if (buildingB == ""){
+			alert("Please select a valid end location");
+			findRoute = false;
+		}
+
+		//Make sure buildings aren't the same
+		if (buildingA == buildingB && buildingA != ""){
+			alert("Your start and end location must be different");
+			findRoute = false;
+		}
+
+		//All is OK above, find the route
+		if (findRoute){
+			dojo.forEach(buildings.features, function(value,index){
+				if (value.attributes.Acronym == buildingA || value.attributes.Acronym == buildingB){
+					//When a building is found add it as a stop
+					addStop(value);
+				}
+			});
+		}
+	});
 }
 
 function addStopFromDeviceLocation(location){
-    require(["esri/geometry/Point","esri/graphic","esri/geometry/webMercatorUtils","esri/SpatialReference","esri/tasks/FeatureSet"],function(Point,Graphic,webMercatorUtils,SpatialReference,FeatureSet){
-        //Put coordinates in text box
-        var coords = $("#coords");
-        x = location.coords.longitude;
-        y = location.coords.latitude;
-        loc = x + "," + y;
-        coords.val(loc);
-        var pt = new Point(x, y, new SpatialReference({ wkid: 4326 }));
-        currentDeviceLoc = new Graphic(pt);
-        if (extentLayer.fullExtent.contains(webMercatorUtils.geographicToWebMercator(currentDeviceLoc.geometry))){
-            addStop(currentDeviceLoc);
-        }
-        else{
-            routeParams.stops = new FeatureSet();
-            results.clear();
-            alert("Your device is reporting a location (" + x + "," + y + ") off campus so we cannot find a route for you. This happens if you're off campus, or on campus using a wired internet connection. Try picking your nearest building instead.");
-        }
-    });
+	require(["esri/geometry/Point","esri/graphic","esri/geometry/webMercatorUtils","esri/SpatialReference","esri/tasks/FeatureSet"],function(Point,Graphic,webMercatorUtils,SpatialReference,FeatureSet){
+		//Put coordinates in text box
+		var coords = $("#coords");
+		x = location.coords.longitude;
+		y = location.coords.latitude;
+		loc = x + "," + y;
+		coords.val(loc);
+		var pt = new Point(x, y, new SpatialReference({ wkid: 4326 }));
+		currentDeviceLoc = new Graphic(pt);
+		if (extentLayer.fullExtent.contains(webMercatorUtils.geographicToWebMercator(currentDeviceLoc.geometry))){
+			addStop(currentDeviceLoc);
+		}
+		else{
+			routeParams.stops = new FeatureSet();
+			results.clear();
+			alert("Your device is reporting a location (" + x + "," + y + ") off campus so we cannot find a route for you. This happens if you're off campus, or on campus using a wired internet connection. Try picking your nearest building instead.");
+		}
+	});
 }
 /**
-	addStop()
-	
-	Takes a graphic and adds it as a stop to the routing problem. When there are two stops added,
-	launch the routing task.
+addStop()
+
+Takes a graphic and adds it as a stop to the routing problem. When there are two stops added,
+launch the routing task.
 */
 function addStop(stop){
 	var barriersToRemove = [];
-	
+
 	//Add the stop to the network
 	s = results.add(new esri.Graphic(esri.geometry.geographicToWebMercator(stop.geometry),stopSymbol));
 	routeParams.stops.features.push(s);
-	
+
 	//When there are two (or more) stops solve the network problem. There should never be more than 2 stops.
 	if (routeParams.stops.features.length >= 2){
-		
+
 		//Reset buffer graphics to match the currently specified fear level
 		bufferGraphics.clear();
-        routeParams.polygonBarriers.features = [];
+		routeParams.polygonBarriers.features = [];
 		var fearval = $("#fearSelector").val();
 		dojo.forEach(nestBuffers[fearval].features,function(nestBuf,index){
 			routeParams.polygonBarriers.features.push(nestBuf);
 			bufferGraphics.add(nestBuf);
 		});
-		
+
 		//Loop through polygon barriers and check if they contain the start or end point. If so, remove them.
 		dojo.forEach(routeParams.polygonBarriers.features,function(barrier,index){
 			if(barrier.geometry.contains(routeParams.stops.features[0].geometry) || barrier.geometry.contains(routeParams.stops.features[1].geometry)){
 				barriersToRemove.push(index);
 			}
 		});
-		
+
 		//Must reverse list of features to remove to avoid screwing up indices
-		dojo.forEach(barriersToRemove.reverse(),function(bIndex){ 
+		dojo.forEach(barriersToRemove.reverse(),function(bIndex){
 			routeParams.polygonBarriers.features.splice(bIndex,1);
 		});
 		if(barriersToRemove.length > 0){
 			alert("Note, your start or end point is close to a goose nest. Proceed with caution.");
 		}
-		
+
 		//Solve the routing problem
 		routeTask.solve(routeParams);
 	}
 }
 
 /**
-	showRoute()
-	
-	Called when the route is successfully solved. Displays resulting route on map
-	and sets the map's extent to show the entire route.
+showRoute()
+
+Called when the route is successfully solved. Displays resulting route on map
+and sets the map's extent to show the entire route.
 */
 function showRoute(solveResult){
 	results.add(solveResult.result.routeResults[0].route.setSymbol(routeSymbol));
 	map.setExtent(solveResult.result.routeResults[0].route.geometry.getExtent(),true);
-    //Modify attribution to include additional sources
-    routeAttribution = routeAttributionText;
+	//Modify attribution to include additional sources
+	routeAttribution = routeAttributionText;
 }
 
 /**
-	routeErrorHandler()
-	Called when something goes wrong with the route.
+routeErrorHandler()
+Called when something goes wrong with the route.
 */
 function routeErrorHandler(err){
-	if (useLocation){				
+	if (useLocation){
 		var bldgB = dojo.byId("buildingSelectorB").value;
 		alert("A route could not be found between (" + currLocation.x + ", " + currLocation.y + ") and " + bldgB);
 	}
@@ -241,7 +241,7 @@ function routeErrorHandler(err){
 		var bldgB = dojo.byId("buildingSelectorB").value;
 		alert("A route could not be found between " + bldgA + " and " + bldgB);
 	}
-		
+
 	routeParams.stops = new esri.tasks.FeatureSet();
 	map.graphics.clear();
 	resetRouting();
--- js/reporting.js
+++ js/reporting.js
@@ -1,8 +1,8 @@
 /**
-	saveFeature()
+saveFeature()
 
-	Takes the values on the nest submission form and supplements them with the global x and y set elsewhere
-	to create a new point in the goose nests feature layer.
+Takes the values on the nest submission form and supplements them with the global x and y set elsewhere
+to create a new point in the goose nests feature layer.
 */
 function saveFeature(){
 	require(["esri/graphic","esri/geometry/Point","esri/geometry/webMercatorUtils","esri/SpatialReference","esri/layers/FeatureLayer"],function(Graphic,Point,webMercatorUtils,SpatialReference,FeatureLayer){
@@ -11,121 +11,133 @@ function saveFeature(){
 		var locDescription = $("#locDesc").val();
 		var submitter = $("#submitter").val();
 		var twitterSub = $("#twitterHandle").val();
-  	var emailAddr = $("#emailAddress").val();
+		var emailAddr = $("#emailAddress").val();
 
 		var currentDate = new Date();
-    var dateString = ((currentDate.getMonth() + 1) + "/" + currentDate.getDate() + "/" + currentDate.getFullYear());
+		var dateString = ((currentDate.getMonth() + 1) + "/" + currentDate.getDate() + "/" + currentDate.getFullYear());
 
 		//Convert global x and y (which are in lat/long) to Web Mercator
 		var p = webMercatorUtils.geographicToWebMercator(new Point(x,y,new SpatialReference({wkid:4326})));
 
 		//Create a graphic from that point and add it to the feature layer
 		var g = new Graphic(p,null,{"description":locDescription,"submitter":submitter,"twitter":twitterSub,"submitdate":dateString,"status":0,"email":emailAddr});
-		gooseEditFL.applyEdits([g],null,null);
+		gooseFL.applyEdits([g],null,null);
 	});
 }
 
 function attachPhoto(ftrEditResult){
-	gooseEditFL.addAttachment(ftrEditResult.adds[0].objectId,document.getElementById("addNestForm"));
+	gooseFL.addAttachment(ftrEditResult.adds[0].objectId,document.getElementById("addNestForm"));
 }
 
 function addNewPhoto(){
-    require(["esri/graphic","esri/geometry/Point","esri/geometry/webMercatorUtils","esri/SpatialReference"],function(Graphic,Point,webMercatorUtils,SpatialReference){
-        var currentDate = new Date();
-        var dateString = ((currentDate.getMonth() + 1) + "/" + currentDate.getDate() + "/" + currentDate.getFullYear());
+	require(["esri/graphic","esri/geometry/Point","esri/geometry/webMercatorUtils","esri/SpatialReference"],function(Graphic,Point,webMercatorUtils,SpatialReference){
+		var currentDate = new Date();
+		var dateString = ((currentDate.getMonth() + 1) + "/" + currentDate.getDate() + "/" + currentDate.getFullYear());
 
-        //Convert global x and y (which are in lat/long) to Web Mercator
-				var p = new Point($("#nestX")[0].value,$("#nestY")[0].value,new SpatialReference({wkid:4326}));
+		//Convert global x and y (which are in lat/long) to Web Mercator
+		var p = new Point($("#nestX")[0].value,$("#nestY")[0].value,new SpatialReference({wkid:4326}));
 
-        var g = new Graphic(p,null,{"status":"0","nestid":$("#nestOID")[0].value,"submitdate":dateString});
-        submittedPicsFL.applyEdits([g],null,null);
-    });
+		var g = new Graphic(p,null,{"status":"0","nestid":$("#nestOID")[0].value,"submitdate":dateString});
+		submittedPicsFL.applyEdits([g],null,null);
+	});
 }
 
 function attachNewPhoto(ftrEditResult){
-    submittedPicsFL.addAttachment(ftrEditResult.adds[0].objectId,document.getElementById("addPhotoToNestForm"));
+	submittedPicsFL.addAttachment(ftrEditResult.adds[0].objectId,document.getElementById("addPhotoToNestForm"),
+	function(results){
+		// alert("success!");
+		$("#photoResponse")[0].innerHTML = "Your photo was succefully submitted. It will be reviewed before being added to the map.";
+		$("#postPhotoSubmissionModal").modal('show');
+	},
+	function(error_results){
+		$("#photoResponse")[0].innerHTML = "<p>There was an issue submitting your photo:" + error_results.details[0] + "</p>";
+		$("#photoResponse")[0].innerHTML += "<p>The most likely cause is that your photo is larger than 10MB in size.</p>";
+
+		$("#postPhotoSubmissionModal").modal('show');
+	}
+);
 }
 
 /**
-	populateLocationFromDevice()
+populateLocationFromDevice()
 
-	Given a position from HTML geolocation this function populates the coords text box,
-	disables editing on it, and notifies the user by changing the button's colour to green.
+Given a position from HTML geolocation this function populates the coords text box,
+disables editing on it, and notifies the user by changing the button's colour to green.
 */
 function populateLocationFromDevice(location){
-    require(["esri/geometry/Point","esri/graphic","esri/geometry/webMercatorUtils","esri/SpatialReference"],function(Point,Graphic,webMercatorUtils,SpatialReference){
-        //Put coordinates in text box
-        var coords = $("#coords");
-        x = location.coords.longitude;
-        y = location.coords.latitude;
-        loc = x + "," + y;
+	require(["esri/geometry/Point","esri/graphic","esri/geometry/webMercatorUtils","esri/SpatialReference"],function(Point,Graphic,webMercatorUtils,SpatialReference){
+		//Put coordinates in text box
+		var coords = $("#coords");
+		x = location.coords.longitude;
+		y = location.coords.latitude;
+		loc = x + "," + y;
 
-        var pt = new Point(x, y, new SpatialReference({ wkid: 4326 }));
-        currentDeviceLoc = new Graphic(pt);
-        if (extentLayer.fullExtent.contains(webMercatorUtils.geographicToWebMercator(currentDeviceLoc.geometry))){
-            coords.val(loc);
-            //Lock down text box and change colour of map icon
-            $("#coords")[0].disabled = true;
-            $("#mapMarker")[0].style.color = "green";
+		var pt = new Point(x, y, new SpatialReference({ wkid: 4326 }));
+		currentDeviceLoc = new Graphic(pt);
+		if (extentLayer.fullExtent.contains(webMercatorUtils.geographicToWebMercator(currentDeviceLoc.geometry))){
+			coords.val(loc);
+			//Lock down text box and change colour of map icon
+			$("#coords")[0].disabled = true;
+			$("#mapMarker")[0].style.color = "green";
 
-            //Enable submission
-            $("#submitNest")[0].disabled = false;
-        }
-        else{
-            if(!alerted){
-                coords.val(loc);
-                alert("Your device is reporting a location that is not on the UW campus. You may only submit nest locations on campus.");
-                alerted = true;
-            }
-            $("#mapMarker")[0].style.color = "red";
-        }
-    });
+			//Enable submission
+			$("#submitNest")[0].disabled = false;
+		}
+		else{
+			if(!alerted){
+				coords.val(loc);
+				alert("Your device is reporting a location that is not on the UW campus. You may only submit nest locations on campus.");
+				alerted = true;
+			}
+			$("#mapMarker")[0].style.color = "red";
+		}
+	});
 }
 
 /**
-	populateLocationFromClick()
+populateLocationFromClick()
 
-	Given a point object set the global x and y to the x and y coordinates of the point.
-	Set the coordinates text box to show the point and disable it.
+Given a point object set the global x and y to the x and y coordinates of the point.
+Set the coordinates text box to show the point and disable it.
 */
 function populateLocationFromClick(pt){
-require(["esri/geometry/webMercatorUtils"],function(webMercatorUtils){
-	x = pt.x;
-	y = pt.y;
-	var coords = $("#coords");
-	var loc = x + "," + y;
-    if (extentLayer.fullExtent.contains(webMercatorUtils.geographicToWebMercator(pt))){
-        coords.val(loc);
-        $("#coords")[0].disabled = true;
-        $("#mapMarker")[0].style.color = "green";
-        $("#addLocationModal").modal('show');
-        //Disable point selection mode so user can interact with the map again
-        disablePointSelection();
+	require(["esri/geometry/webMercatorUtils"],function(webMercatorUtils){
+		x = pt.x;
+		y = pt.y;
+		var coords = $("#coords");
+		var loc = x + "," + y;
+		if (extentLayer.fullExtent.contains(webMercatorUtils.geographicToWebMercator(pt))){
+			coords.val(loc);
+			$("#coords")[0].disabled = true;
+			$("#mapMarker")[0].style.color = "green";
+			$("#addLocationModal").modal('show');
+			//Disable point selection mode so user can interact with the map again
+			disablePointSelection();
 
-        //Enable submission
-        $("#submitNest")[0].disabled = false;
-    }
-    else{
-        alert("That point is not on the UWaterloo campus. Please try another point.");
-    }
-});
+			//Enable submission
+			$("#submitNest")[0].disabled = false;
+		}
+		else{
+			alert("That point is not on the UWaterloo campus. Please try another point.");
+		}
+	});
 }
 
 /**
-	enablePointSelection()
+enablePointSelection()
 
-	Sets a flag that enables the user to click on a point on the map and have the resulting x,y coordinate
-	used to report a new nest location. This is checked in the map's click handler.
+Sets a flag that enables the user to click on a point on the map and have the resulting x,y coordinate
+used to report a new nest location. This is checked in the map's click handler.
 */
 function enablePointSelection(){
 	addPointMode = true;
 }
 
 /**
-	disablePointSelection()
+disablePointSelection()
 
-	Sets a flag that disables the user's ability to click on the map to create a nest location.
-	This is checked in the map's click handler.
+Sets a flag that disables the user's ability to click on the map to create a nest location.
+This is checked in the map's click handler.
 */
 function disablePointSelection(){
 	addPointMode = false;
